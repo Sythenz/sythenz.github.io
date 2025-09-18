63e548e8f40d722cb7f01b241f5cda6d96e5ce64
Sythenz <alessafur@protonmail.com>
2025-09-18 22:14:59 +0200

    Clean slate for Eleventy migration

--- codekitten.me/content/about.md
+++
@@ -1,5 +0,0 @@
-+++
-title = 'About'
-date = 2024-04-16T13:45:52+02:00
-draft = true
-+++
--- codekitten.me/themes/codekitten2024theme/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 200) {
-        $("header").addClass("darkHeader");
-    } else {
-        $("header").removeClass("darkHeader");
-    }
-});
