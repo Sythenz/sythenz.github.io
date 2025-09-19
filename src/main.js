8949808c5a1aaaa33869c1b0888b272c44db0d6e
Sythenz <alessafur@protonmail.com>
2025-09-20 00:59:12 +0200

    Just fixing up some hero and header layouting.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const header = document.getElementById("header");
-  const checkbox = document.getElementById("sidebarToggle");
+	const header = document.getElementById("header");
+	const checkbox = document.getElementById("sidebarToggle");
 
-  if (!header || !checkbox) return;
+	if (!header || !checkbox) return;
 
-  function onScroll() {
-    if(checkbox.checked) return; // Do nothing if sidebar is open
+	function onScroll() {
+		if(checkbox.checked) return; // Do nothing if sidebar is open
+			if (window.scrollY > 100) {
+			header.classList.remove("header-hero");
+		} else {
+			header.classList.add("header-hero");
+		}
+	}
 
-    if (window.scrollY > 100) {
-      header.classList.add("header-scrolled");
-      header.classList.remove("header");
-    } else {
-      header.classList.remove("header-scrolled");
-      header.classList.add("header");
-    }
-  }
-  
-  window.addEventListener("scroll", onScroll);
-  onScroll(); // Set initial state
+	window.addEventListener("scroll", onScroll);
+	onScroll(); // Set initial state
 });
